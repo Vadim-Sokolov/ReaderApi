9498b57cb9543f7f9b5e33af8d6dde859ea19854
Vadim-Sokolov <vadim.sokolov@gmail.com>
2023-12-30 08:49:55 +0000

    Initial commit

---
+++ src/main/java/com/esg/readerapi/ReaderapiApplication.java
@@ -0,0 +1,13 @@
+package com.esg.readerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaderapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaderapiApplication.class, args);
+	}
+
+}
